f3abfe3f6576eab69e95f6d8753daaaeccaede56
Simon <simon@systemli.org>
2020-06-09 21:55:27 +0200

    Adding overlay.svg to app folder

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../templates/overlay.svg">
+    <HelloWorld msg=""/>
   </div>
 </template>
 
