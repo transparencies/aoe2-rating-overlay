60983853c927f638aeae8e96914f0ee668eb1ca4
Simon <simon@systemli.org>
2020-06-11 15:55:09 +0200

    Basic functionality to create a window

--- client/app-rust/src/main.rs
+++ client/app-rust/src/main.rs
@@ -1,40 +1,66 @@
+#![windows_subsystem = "windows"]
 
-use web_view::*;
+// use web_view::*;
+use neutrino::{App, Window};
+use neutrino::widgets::label::Label;
+use neutrino::widgets::button::Button;
 
 #[macro_use]
 extern crate clap;
-use clap::App;
+use clap::App as OtherApp;
+
+// mod download;
 
 fn main() {
     let yaml = load_yaml!("../cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = OtherApp::from_yaml(yaml).get_matches();
 
-    println!("Steam-ID set to: {}", matches.value_of("steamid").unwrap());
+    // web_view::builder()
+    //     .title("")
+    //     .content(Content::Html(HTML))
+    //     .size(300, 70)
+    //     .frameless(false)
+    //     .debug(true)
+    //     .user_data("")
+    //     .invoke_handler(|webview, arg| {
+    //         match arg {
+    //             "exit" => webview.exit(),
+    //             _ => (),
+    //         }
+    //         Ok(())
+    //     })
+    //     .run()
+    //     .unwrap();
 
-    web_view::builder()
-        .title("")
-        .content(Content::Html(HTML))
-        .size(150, 150)
-        .frameless(true)
-        .debug(true)
-        .user_data("")
-        .invoke_handler(|webview, arg| {
-            match arg {
-                "exit" => webview.exit(),
-                _ => (),
-            }
-            Ok(())
-        })
-        .run()
-        .unwrap();
+let mut label = Label::new("my_label");
+label.set_text(&("Your Steam-ID is: ".to_owned() + matches.value_of("steamid").unwrap()));
+
+let mut button = Button::new("my_button");
+button.set_text("Exit");
+
+
+let mut window = Window::new();
+window.set_title("aoe2overlay");
+window.set_size(300,70);
+// window.set_frameless();
+window.set_debug();
+
+// window.set_child(Box::new(label));
+window.set_child(Box::new(button));
+
+App::run(window);
+
+
+// TODO: remember last used steamid (parse from file) 
+
 }
 
-const HTML: &str = r#"
-<!doctype html>
-<html>
-  <body>
-        <object data="../assets/overlay.svg" type="image/svg+xml"></object>
-        <button onclick="external.invoke('exit')" style="display:block;width:100px;height:100px;font-size:24pt;margin:25px auto;">exit</button>
-  </body>
-</html>
-"#;
+// const HTML: &str = r#"
+// <!doctype html>
+// <html>
+//   <body>
+//         <object data="../assets/overlay.svg" type="image/svg+xml"></object>
+//         <button onclick="external.invoke('exit')" style="display:block;width:80px;height:15px;font-size:12pt;margin:25px auto;">exit</button>
+//   </body>
+// </html>
+// "#;
