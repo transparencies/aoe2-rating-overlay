91ef5bc9c6438d7fc9f0631e83bd03c3b79976fa
Simon <simon@systemli.org>
2020-06-11 12:39:38 +0200

    Configured web-view

--- client/app-rust/src/main.rs
+++ client/app-rust/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let yaml = load_yaml!("../cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    println!("Using input file: {}", matches.value_of("steam_id").unwrap());
+    println!("Steam-ID set to: {}", matches.value_of("steamid").unwrap());
 
     web_view::builder()
         .title("")
@@ -33,6 +33,7 @@ const HTML: &str = r#"
 <!doctype html>
 <html>
   <body>
+        <object data="../assets/overlay.svg" type="image/svg+xml"></object>
         <button onclick="external.invoke('exit')" style="display:block;width:100px;height:100px;font-size:24pt;margin:25px auto;">exit</button>
   </body>
 </html>
